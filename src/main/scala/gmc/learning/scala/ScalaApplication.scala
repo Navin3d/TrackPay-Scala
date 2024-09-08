75dc6545f1155e5d503b50df78018b53e21c1814
navin3d <smnavin65@gmail.com>
2024-09-09 01:49:27 +0530

    Initial Commit

---
+++ src/main/scala/gmc/learning/scala/ScalaApplication.scala
@@ -0,0 +1,11 @@
+package gmc.learning.scala
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ScalaApplication
+
+@main def main() : Unit = {
+    SpringApplication.run(classOf[ScalaApplication])
+}
---
+++ src/test/scala/gmc/learning/scala/ScalaApplicationTests.scala
@@ -0,0 +1,13 @@
+package gmc.learning.scala
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.Test
+
+@SpringBootTest
+class ScalaApplicationTests {
+
+  @Test
+  def contextLoads(): Unit = {
+
+  }
+}
